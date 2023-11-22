2d39a14233b72d6fb7975b85fb2c0d8a388b3de7
Eva van Dongen <144825696+orphansocks@users.noreply.github.com>
2023-11-22 18:01:54 +0100

    Update src/helpers/originalStock.js
    Co-authored-by: Zwen1111 <zwen12@msn.com>

--- src/helpers/originalStock.js
+++ src/helpers/originalStock.js
@@ -1,7 +1,7 @@
 function originalStock(inventoryArray) {
     let purchased = 0
     for (let i = 0; i < inventoryArray.length; i++) {
-        purchased = purchased + inventoryArray[i].originalStock;
+        purchased += inventoryArray[i].originalStock;
     }
     return purchased
 }
